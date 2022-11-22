771173efb933a161921b5e4add5c8ca60e4c8399
Atreyo073 <107910400+Atreyo073@users.noreply.github.com>
2022-11-22 21:28:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function green()
+{
+    document.getElementById("green").style.display="none";
+    document.getElementById("green.png").style.display="inline-block";
+}
+function blue()
+{
+    document.getElementById("green").style.display="none";
+    document.getElementById("blue.png").style.display="inline-block";
+}
+function red()
+{
+    document.getElementById("green").style.display="none";
+    document.getElementById("red.png").style.display="inline-block";
+}
+function white()
+{
+    document.getElementById("green").style.display="none";
+    document.getElementById("white.png").style.display="inline-block";
+}
+function orange()
+{
+    document.getElementById("green").style.display="none";
+    document.getElementById("orange.png").style.display="inline-block";
+}
+function yellow()
+{
+    document.getElementById("green").style.display="none";
+    document.getElementById("yellow.png").style.display="inline-block";
+}
